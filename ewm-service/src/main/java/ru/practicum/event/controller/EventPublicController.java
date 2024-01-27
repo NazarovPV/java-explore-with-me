2c1a29d670c2fac6bdb220d683c0a159a3fad551
NazarovPV <nazar969@yandex.ru>
2024-01-27 14:38:30 +0300

    Feat

--- ewm-service/src/main/java/ru/practicum/event/controller/EventPublicController.java
+++ ewm-service/src/main/java/ru/practicum/event/controller/EventPublicController.java
@@ -3,7 +3,6 @@ package ru.practicum.event.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.event.EventService;
 import ru.practicum.event.dto.EventFullDto;
@@ -18,7 +17,6 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(path = "/events")
-@Validated
 public class EventPublicController {
 
     public final EventService eventService;
